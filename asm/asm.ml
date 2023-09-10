02872be5e97a65ca1025375fbcb9c8999456146c
Rob Lewis <Rob.y.lewis@gmail.com>
2023-09-10 18:30:09 -0400

    autoformat

--- asm/asm.ml
+++ asm/asm.ml
@@ -7,8 +7,10 @@ type operand = Reg of register | Imm of int
 let is_register o = match o with Reg _ -> true | _ -> false
 
 let string_of_operand = function
-  | Reg r -> string_of_register r
-  | Imm i -> string_of_int i
+  | Reg r ->
+      string_of_register r
+  | Imm i ->
+      string_of_int i
 
 type directive =
   | Global of string
@@ -24,13 +26,15 @@ let run cmd args =
   let open Shexp_process.Infix in
   eval (run cmd args |- read_all)
 
-let macos = run "uname" [ "-s" ] |> String.trim |> String.equal "Darwin"
+let macos = run "uname" ["-s"] |> String.trim |> String.equal "Darwin"
 
 let string_of_directive = function
   (* frontmatter *)
-  | Global l -> Printf.sprintf (if macos then "global _%s" else "global %s") l
+  | Global l ->
+      Printf.sprintf (if macos then "global _%s" else "global %s") l
   (* labels *)
-  | Label l -> Printf.sprintf (if macos then "_%s:" else "%s:") l
+  | Label l ->
+      Printf.sprintf (if macos then "_%s:" else "%s:") l
   (* actual instructions *)
   | Mov (dest, src) ->
       Printf.sprintf "\tmov %s, %s" (string_of_operand dest)
@@ -41,5 +45,7 @@ let string_of_directive = function
   | Sub (dest, src) ->
       Printf.sprintf "\tsub %s, %s" (string_of_operand dest)
         (string_of_operand src)
-  | Ret -> "\tret"
-  | Comment s -> Printf.sprintf "; %s" s
+  | Ret ->
+      "\tret"
+  | Comment s ->
+      Printf.sprintf "; %s" s
--- lib/compile.ml
+++ lib/compile.ml
@@ -1,19 +1,16 @@
-let compile (program: string) : string =
-  String.concat "\n" 
-  ["global entry";
-  "entry:";
-  Printf.sprintf "\tmov rax, %s" program;
-  "\tret"]
+let compile (program : string) : string =
+  String.concat "\n"
+    ["global entry"; "entry:"; Printf.sprintf "\tmov rax, %s" program; "\tret"]
 
-let compile_to_file (program: string): unit =
+let compile_to_file (program : string) : unit =
   let file = open_out "program.s" in
-  output_string file (compile program);
+  output_string file (compile program) ;
   close_out file
 
-let compile_and_run (program: string): string =
-  compile_to_file program;
-  ignore (Unix.system "nasm program.s -f elf64 -o program.o");
-  ignore (Unix.system "gcc program.o runtime.o -o program -z noexecstack");
+let compile_and_run (program : string) : string =
+  compile_to_file program ;
+  ignore (Unix.system "nasm program.s -f elf64 -o program.o") ;
+  ignore (Unix.system "gcc program.o runtime.o -o program") ;
   let inp = Unix.open_process_in "./program" in
   let r = input_line inp in
-  close_in inp; r
+  close_in inp ; r
