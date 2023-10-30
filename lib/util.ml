56a51379fdfcd4647d2581ef8dc0b269decdb3f3
Rob Lewis <Rob.y.lewis@gmail.com>
2023-10-29 22:43:27 -0400

    labeling function

--- lib/util.ml
+++ lib/util.ml
@@ -21,3 +21,30 @@ module Symtab = struct
 end
 
 type 'a symtab = 'a Symtab.t
+
+let rec input_all (ch : in_channel) : string =
+  try
+    let c = input_char ch in
+    String.make 1 c ^ input_all ch
+  with End_of_file -> ""
+
+let defn_label s =
+  let nasm_char c =
+    match c with
+    | 'a' .. 'z'
+    | 'A' .. 'Z'
+    | '0' .. '9'
+    | '_'
+    | '$'
+    | '#'
+    | '@'
+    | '~'
+    | '.'
+    | '?' ->
+        c
+    | _ ->
+        '_'
+  in
+  Printf.sprintf "function_%s_%d"
+    (String.map nasm_char s)
+    (Hashtbl.hash s)
