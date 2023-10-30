0a90480ea30b39c33767762ffedcf65a48be7d83
Rob Lewis <Rob.y.lewis@gmail.com>
2023-10-29 20:38:17 -0400

    update symtab

--- lib/util.ml
+++ lib/util.ml
@@ -5,10 +5,19 @@ let gensym : string -> string =
     counter := !counter + 1 ;
     symbol
 
-module Symtab = Map.Make (struct
+module ST = Map.Make (struct
   type t = string
 
   let compare = compare
 end)
+
+module Symtab = struct
+  include ST
+
+  let of_list l = l |> List.to_seq |> of_seq
+
+  let add_list tab l =
+    List.fold_left (fun tab (k, v) -> add k v tab) tab l
+end
 
 type 'a symtab = 'a Symtab.t
