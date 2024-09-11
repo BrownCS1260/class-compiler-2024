361df9072afdecaa670d55bc1fce0f2e81e8523e
Rob Lewis <Rob.y.lewis@gmail.com>
2024-09-11 11:37:39 -0400

    after class Sep 9

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,10 +1,19 @@
-let compile (program : string) : string =
-  String.concat "\n"
-    ["global entry"; "entry:"; Printf.sprintf "\tmov rax, %s" program; "\tret"]
+open S_exp
+
+exception BadExpression of s_exp
+
+let compile (program : s_exp) : string =
+  match program with
+  | Num n ->
+      String.concat "\n"
+        ["global entry"; "entry:"; Printf.sprintf "\tmov rax, %d" n; "\tret"]
+  | _ ->
+      raise (BadExpression program)
 
 let compile_to_file (program : string) : unit =
   let file = open_out "program.s" in
-  output_string file (compile program) ;
+  let prog = parse program in
+  output_string file (compile prog) ;
   close_out file
 
 let compile_and_run (program : string) : string =
