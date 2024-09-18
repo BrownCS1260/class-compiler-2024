1e640e29658e652ed4bb7e3b6b234c4d96ad64db
Rob Lewis <Rob.y.lewis@gmail.com>
2024-09-18 13:18:12 -0400

    status after Sep 16

--- lib/compile.ml
+++ lib/compile.ml
@@ -3,14 +3,30 @@ open Asm
 
 exception BadExpression of s_exp
 
+let num_shift = 2
+
+let num_mask = 0b11
+
+let num_tag = 0b00
+
+let bool_shift = 7
+
+let bool_mask = 0b1111111
+
+let bool_tag = 0b0011111
+
 let rec compile_exp (program : s_exp) : directive list =
   match program with
   | Num n ->
-      [Mov (Reg Rax, Imm n)]
+      [Mov (Reg Rax, Imm (n lsl num_shift))]
+  | Sym "true" ->
+      [Mov (Reg Rax, Imm ((1 lsl bool_shift) lor bool_tag))]
+  | Sym "false" ->
+      [Mov (Reg Rax, Imm ((0 lsl bool_shift) lor bool_tag))]
   | Lst [Sym "add1"; exp] ->
-      compile_exp exp @ [Add (Reg Rax, Imm 1)]
+      compile_exp exp @ [Add (Reg Rax, Imm (1 lsl num_shift))]
   | Lst [Sym "sub1"; exp] ->
-      compile_exp exp @ [Sub (Reg Rax, Imm 1)]
+      compile_exp exp @ [Sub (Reg Rax, Imm (1 lsl num_shift))]
   | _ ->
       raise (BadExpression program)
 
--- lib/interp.ml
+++ lib/interp.ml
@@ -2,16 +2,49 @@ open S_exp
 
 exception BadExpression of s_exp
 
-let rec interp_exp (exp : s_exp) : int =
+type value = Number of int | Boolean of bool
+
+let string_of_value (v : value) : string =
+  match v with
+  | Number n ->
+      string_of_int n
+  | Boolean b ->
+      string_of_bool b
+
+let rec interp_exp (exp : s_exp) : value =
   match exp with
   | Num n ->
-      n
-  | Lst [Sym "add1"; exp] ->
-      interp_exp exp + 1
-  | Lst [Sym "sub1"; exp] ->
-      interp_exp exp - 1
+      Number n
+  | Sym "true" ->
+      Boolean true
+  | Sym "false" ->
+      Boolean false
+  | Lst [Sym "add1"; exp] -> (
+    match interp_exp exp with
+    | Number n ->
+        Number (n + 1)
+    | _ ->
+        raise (BadExpression exp) )
+  | Lst [Sym "sub1"; exp] -> (
+    match interp_exp exp with
+    | Number n ->
+        Number (n - 1)
+    | _ ->
+        raise (BadExpression exp) )
+  | Lst [Sym "not"; arg] ->
+      if interp_exp arg = Boolean false then Boolean true
+      else Boolean false
+  | Lst [Sym "zero?"; arg] ->
+      if interp_exp arg = Number 0 then Boolean true
+      else Boolean false
+  | Lst [Sym "num?"; arg] -> (
+    match interp_exp arg with
+    | Number _ ->
+        Boolean true
+    | _ ->
+        Boolean false )
   | _ ->
       raise (BadExpression exp)
 
 let interp (program : string) : string =
-  parse program |> interp_exp |> string_of_int
+  parse program |> interp_exp |> string_of_value
